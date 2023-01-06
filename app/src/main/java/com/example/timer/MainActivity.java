32576ff0d2100009caa5d0cfef927e607442b029
Jinamizi <tonnylango@gmail.com>
2023-01-06 23:44:13 +0300

    j

--- app/src/main/java/com/example/timer/MainActivity.java
+++ app/src/main/java/com/example/timer/MainActivity.java
@@ -9,6 +9,8 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity {
 
     TextView display;
@@ -29,15 +31,35 @@ public class MainActivity extends AppCompatActivity {
     private void startTimer(){
         //update button text
         button.setText("Stop");
+        HashMap<String, Integer> time = new HashMap<String, Integer>();
+        String timeString = display.getText().toString();
+        String [] timeValues = timeString.split(":");
+        String [] timeValue = timeString.split(":");
+        time.put("Hour", Integer.valueOf(timeValue[0]));
+        time.put("Minutes", Integer.valueOf(timeValue[1]));
+        time.put("Seconds", Integer.valueOf(timeValue[2]));
 
         while (running){ //TODO: check on variables that can be accessed by multiple threads
             try{
                 Thread.sleep(1000);
-                
+
             }catch (Exception e){
                 Toast errorToast = Toast.makeText(MainActivity.this, "Error, Some error occurred {e.getMessage()}", Toast.LENGTH_SHORT);
                 errorToast.show();
             }
         }
     }
+
+    private void updateDisplay(){
+        HashMap<String, Integer> time = new HashMap<String, Integer>();
+        String timeString = display.getText().toString();
+        String [] timeValues = timeString.split(":");
+        String [] timeValue = timeString.split(":");
+        timeValue[2]
+        time.put("Hour", Integer.valueOf(timeValue[0]));
+        time.put("Minutes", Integer.valueOf(timeValue[1]));
+        time.put("Seconds", Integer.valueOf(timeValue[2]));
+    }
+
+
 }
